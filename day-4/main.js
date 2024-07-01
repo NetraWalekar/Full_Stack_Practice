2f06e357a5c17e7fc0bb1a73f5f7685b19600141
NetraWalekar <netrawalekar6@gmail.com>
2024-07-01 17:29:13 +0530

    updates

---
+++ day-4/main.js
@@ -0,0 +1,14 @@
+//arrays 
+
+const colors = ['pink','white','blue']
+console.log(colors)
+colors.push('orange');
+console.log(colors[1])
+
+const myArray = [1,true , 'hello']
+myArray.push({name : 'netra'})
+console.log(myArray.indexOf(1))
+console.log(myArray.indexOf('hi'))
+
+myArray.pop();
+console.log(myArray)
